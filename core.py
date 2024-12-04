9ed324d6c47fb5d6722251e386aa24a0658afbe7
Bilal-Alhasan <118555384+Bilal-Alhasan@users.noreply.github.com>
2024-12-04 04:15:29 +0100

    new op

--- core.py
+++ core.py
@@ -1,6 +1,6 @@
-def op():
-    a=input(int("enter the parameter"))
-    b=input(int("enter the parameter"))
+def op(a,b):
+   # a=input(int("enter the parameter"))
+   # b=input(int("enter the parameter"))
     c=a+b
     return c
 
